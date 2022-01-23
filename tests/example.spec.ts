cea44579724690970704c5cd42adf6fcbcd4a7c6
Guilherme de Lemos <guilherme.eti@gmail.com>
2022-01-23 14:34:32 -0300

    Update Playwright Test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+  await page.locator('text=Get started').click();
+  await expect(page).toHaveTitle(/Getting started/);
 });
