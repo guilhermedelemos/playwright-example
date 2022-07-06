91e6336c9ebec5e82abc946d9b2e8e2c8207d846
Guilherme de Lemos <guilherme.eti@gmail.com>
2022-07-06 00:42:42 -0300

    Upgrade Playwright version

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,55 +1,78 @@
-import { PlaywrightTestConfig, devices } from '@playwright/test';
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-
   testDir: './tests',
-
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
-
   expect: {
-
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
     timeout: 5000
   },
-
+  /* Run tests in files in parallel */
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-
+  // reporter: 'html',
+  reporter: process.env.CI ? [
+    // ['dot'],
+    ['github'],
+    ['html', { open: 'never', /*outputFolder: 'my-report'*/ }]
+  ] : [
+    // ['dot'],
+    ['list'],
+    ['html', { open: 'on-failure', outputFolder: 'test-results/report' }],
+    ['json', {  outputFile: 'test-results/report/results.json' }],
+    ['junit', {
+      // Whether to add <properties> with all annotations; default is false
+      embedAnnotationsAsProperties: true,
+    
+      // By default, annotation is reported as <property name='' value=''>.
+      // These annotations are reported as <property name=''>value</property>.
+      textContentAnnotations: ['test_description'],
+    
+      // This will create a "testrun_evidence" property that contains all attachments. Each attachment is added as an inner <item> element.
+      // Disables [[ATTACHMENT|path]] in the <system-out>.
+      embedAttachmentsAsProperty: 'testrun_evidence',
+    
+      // Where to put the report.
+      outputFile: 'test-results/report/results.xml'
+    }],
+    ['./reporters/custom-reporter.ts']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-
+    screenshot: 'on', // on, off, only-on-failure
+    video: 'on', // on, off, retain-on-failure, on-first-retry
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on', // on, off, retain-on-failure, on-first-retry
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-
-      /* Project-specific settings. */
       use: {
         ...devices['Desktop Chrome'],
       },
@@ -70,18 +93,18 @@ const config: PlaywrightTestConfig = {
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: {
+        ...devices['iPhone 12'],
+      },
+    },
 
     /* Test against branded browsers. */
     // {
@@ -99,7 +122,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'test-results/artifacts/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
@@ -107,4 +130,5 @@ const config: PlaywrightTestConfig = {
   //   port: 3000,
   // },
 };
+
 export default config;
